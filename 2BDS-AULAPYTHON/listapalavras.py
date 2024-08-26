7f719a3ee827cb6e36f2e446f0449a263f875d92
abnerlu014 <abnerlucas0114@gmail.com>
2024-08-26 18:02:07 -0300

    2BDS-AULAPYTHON

---
+++ 2BDS-AULAPYTHON/listapalavras.py
@@ -0,0 +1,18 @@
+#lISTA DE PALAVRAS
+listaDePalavras=["Evellyn", "Kathleen", "Abner", "Náthally", "João", "Thayane"]
+print(listaDePalavras)
+#ADICIONAR
+listaDePalavras.append("Jesus")
+print(listaDePalavras)
+#INVERTER
+listaDePalavras.reverse()
+print(listaDePalavras)
+#ORDEM CRESCENTE
+listaDePalavras.sort()
+print(listaDePalavras)
+#ORDEM DECRESCENTE
+listaDePalavras.sort(reverse=True)
+print(listaDePalavras)
+print("--------------------------------")
+#LISTAS FUNÇÕES PYTHON LISTA
+listaDeComandos=["remove","append", "clear", "reverse", "sort", "extend"]
